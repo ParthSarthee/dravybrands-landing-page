a4e15a09add7e0874a9d2c8c7a34164efa80a9ed
Pranjal Mishra <ppranjalmishra@gmail.com>
2024-07-20 15:35:56 +0530

    Changed Link to a tag.

--- app/school/page.jsx
+++ app/school/page.jsx
@@ -127,12 +127,12 @@ function TimelineCard({ icon, title, body }) {
         {body}
       </p>
       <div className="mt-6">
-        <Link
+        <a
           href="#applicationprocess"
           className="px-4 py-2 bg-blue-500 text-white rounded-full text-sm font-semibold transition duration-300 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
         >
           Learn More
-        </Link>
+        </a>
       </div>
     </div>
   );
